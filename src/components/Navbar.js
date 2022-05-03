5a6d6b1931e71e74d16a24fcba93ee3fa51e3ed3
Jyoti <jyotikiran12@gmail.com>
2022-05-04 02:45:09 +0530

    Add style to the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react'
-
+import './navbar.css';
 function Navbar() {
   return (
     <div>
       <nav className="navbar">
-        <div className="navbar-container">
-          <a href="#" className="navbar-logo">
+        <div className="navbar-container d-sm-flex">
+          <a href="#" className="navbar-logo me-auto">
             <img src="https://upugo.in/wp-content/uploads/2020/10/upUgo_logo.png"></img>
           </a>
           <div className="navbar-icons">
-            <ul>
-              <li><i className="fa-solid fa-bell"></i></li>
-              <li><i className="fa-solid fa-user"></i></li>
+            <ul className="d-sm-flex navbar-icons-list">
+              <li className="navbar-icons-item1 p-4"><i className="fa-solid fa-bell"></i></li>
+              <li className="navbar-icons-item2 px-2"><i className="fa-solid fa-user"></i></li>
             </ul>
           </div>
         </div>
